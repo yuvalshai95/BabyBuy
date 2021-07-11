28316cd607f3a5e450bb3714c24559f9ed0e1c30
yuvalshai95 <62174154+yuvalshai95@users.noreply.github.com>
2021-07-11 14:27:05 +0300

    DB_Config  Connection Method

---
+++ DataBase/DB_Config.php
@@ -0,0 +1,71 @@
+<!-- Method1 Connect MySQL Database with PHP Using PDO-->
+<?php
+class Database
+{
+    // Creating database variables
+    private $host = "localhost";
+    private $db_name = "db";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    public function Open_DB_Connection()
+	{
+
+	    $this->conn = null; //reseting the PDO connection to make sure there is no prev connection
+        try //Trying to connect the server using variables
+		    {
+          $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+                // set the PDO error mode to exception
+    			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+		    catch(PDOException $exception) //Could not connect to the server
+		    {
+            echo "Connection error: " . $exception->getMessage(); //print error msg
+        }
+
+        return $this->conn;
+    }
+
+    public function Close_DB_Connection(){
+        $this->$conn=null; //close the conneection with the server
+     }
+}
+
+?>
+
+<!-- Method2 MySQLi Procedural Query-->
+<?php
+class Database
+{
+// Creating database variables
+    private $host = "localhost"; 
+    private $db_name = "babybuydb";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    public function Open_DB_Connection()
+	{
+
+	    $this->conn = null;
+        try //Trying to connect the server using variables
+		    {
+          $this->conn = mysqli_connect($host,$username,$password,$db_name);
+    		
+		    catch(Exception $e) //Could not connect to the server
+		    {
+            echo "Connection error message: " . $e->getMessage(); //print error msg
+        }
+
+        return $this->conn; //opening the connection with the server
+    }
+}
+
+public function Close_DB_Connection(){
+   $this->$conn->close(); //close the conneection with the server
+}
+
+?>
+
+
