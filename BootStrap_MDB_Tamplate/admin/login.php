45aafd58e96557a65eb6f3a9538b4519467795cc
yuvalshai95 <62174154+yuvalshai95@users.noreply.github.com>
2021-07-12 15:32:18 +0300

    Session

--- BootStrap_MDB_Tamplate/admin/login.php
+++ BootStrap_MDB_Tamplate/admin/login.php
@@ -1,3 +1,18 @@
+<?php include_once '../Classes/Admin.php'; ?>
+
+<?php 
+$admin = new Admin(); 
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+	$adminEmail = $_POST['Email']
+	$adminPass = $_POST['UserPassword']
+
+	$loginCheck = $admin->checkLoginInfo($adminEmail,$adminPass);
+}
+
+
+
+?>
+
 <!DOCTYPE html>
 <head>
 <meta charset="utf-8">
@@ -7,22 +22,28 @@
 <body>
 <div class="container">
 	<section id="content">
+
+
 		<form action="" method="post">
 			<h1>Admin Login</h1>
 			<div>
-				<input type="text" placeholder="Username" required="" name="username"/>
+				<input type="text" placeholder="Email" required="" name="Email"/>
 			</div>
 			<div>
-				<input type="password" placeholder="Password" required="" name="password"/>
+				<input type="password" placeholder="Password" required="" name="UserPassword"/>
 			</div>
 			<div>
 				<input type="submit" value="Log in" />
 			</div>
 		</form><!-- form -->
+
 		<div class="button">
-			<a href="#">Training with live project</a>
+
+			<a href="#">Yuval & Adi Project</a>
 		</div><!-- button -->
+
 	</section><!-- content -->
+
 </div><!-- container -->
 </body>
 </html>
---
+++ DataBase/Session.php
@@ -0,0 +1,28 @@
+<?php
+
+class Session{
+    public static function init(){
+        session_start();
+
+    }
+
+    public static function setSessionKeyValue($key,$value){
+        $_SESSION[$key] = $value;
+    }
+
+    public static function getSessionKey($key){
+        if(isset($_SESSION[$key])){
+            return $_SESSION[$key]
+        }
+        else{
+            return false;
+    }
+    }
+
+    public static function destroySession(){
+        session_decode();
+        header("Location:login.php");
+    }
+}
+
+?>
