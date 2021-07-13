68475ee3c01838718deb3d4e24fd81016066164c
yuvalshai95 <62174154+yuvalshai95@users.noreply.github.com>
2021-07-13 13:09:43 +0300

    dashbord comments

--- admin/dashbord.php
+++ admin/dashbord.php
@@ -3,8 +3,8 @@
 <?php 
 $admin = new Admin(); 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
-	$adminEmail = $_POST['Email'];
-	$adminPass = $_POST['UserPassword'];
+	$adminEmail = $_POST['Email']; // input from the form
+	$adminPass = $_POST['UserPassword']; // input from the form
 	$loginCheck = $admin->checkLoginInfo($adminEmail,$adminPass);
 }
 ?>
@@ -19,18 +19,19 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 <div class="container">
 	<section id="content">
 
-
+		<!-- form -->
 		<form action="" method="post">
 			<h1>Admin Login</h1>
 
+			<!-- Login Check msg-->
 			<span style="color:red; font-size: 18px;"> 
 			<?php
 				if (isset($loginCheck)) {
 					echo $loginCheck;
 				}
 			?>
-		</span>
-
+			</span>
+			<!--/Login Check msg-->
 
 			<div>
 				<input type="text" placeholder="Email" name="Email"/>
@@ -41,7 +42,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 			<div>
 				<input type="submit" value="Log in" />
 			</div>
-		</form><!-- form -->
+		</form><!--/ form -->
 
 		<div class="button">
 
