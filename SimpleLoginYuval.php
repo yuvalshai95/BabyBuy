214435497676337f45fdda1fcee194cf09792c8e
Adi Hemo <adi4hemo@gmail.com>
2021-07-30 11:12:12 +0300

    tooltip for login page

--- SimpleLoginYuval.php
+++ SimpleLoginYuval.php
@@ -49,13 +49,11 @@
                         <div class="form-group">
 
                             <button class="btn btn-class">Login</button>
-                            <label for="" >Forgot Password?</label>
+                            <label class="forgotPass" for="" id="nav-toggle-button" data-toggle="tooltip" 
+                                    title="Relax and try to remember your password" data-placement="bottom">Forgot Password?</label>
                              
                         </div>
 
-                        
-                       
-                        
                     </form>
              </div>
         </div>
@@ -74,6 +72,12 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>   
     <!-- bootstrap END -->
+
+    <script>
+        $(function () {
+            $("[data-toggle='tooltip']").tooltip();
+        });
+    </script>
 
 </body>
 </html>
