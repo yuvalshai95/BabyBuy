9dbce25da7fcf96ce6eb6cb8e8ce5efbe7918221
Adi Hemo <adi4hemo@gmail.com>
2021-07-31 20:42:59 +0300

    Start of article list page

---
+++ ArticlesList.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Articles List</title>
+    <link href="styleA/css/all.css" rel="stylesheet">
+    <link href="styleA/css/all.min.css" rel="stylesheet">
+    <!-- style Articles List -->
+    <link href="styleA/articlesListStyle.css" rel="stylesheet">
+</head>
+<body>
+    
+<div class="content clearfix">
+    <div class="main-content">
+        <h1 class="recent-post-title">Recent Articles</h1>
+
+        <div class="post">
+            <img src="img/article1.png" alt="" class="post-image">
+            <div class="post-preview">
+                <h2><a href="#">Title Text Text Text</a></h2>
+                <i class="far calendar">Jul 31, 2021</i>
+                <p class="preview-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolor suscipit 
+                    libero eos atque quia ipsa sint voluptatibus! 
+                    Beatae sit assumenda asperiores iure at maxime atque repellendus maiores quia sapiente.
+                </p>
+                <a href="#" class="readMoreBTN read-more">Read More</a>
+            </div>
+        </div>
+
+        <div class="post">
+            <img src="img/article1.png" alt="" class="post-image">
+            <div class="post-preview">
+                <h2><a href="#">Title Text Text Text</a></h2>
+                <i class="far calendar">Jul 31, 2021</i>
+                <p class="preview-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolor suscipit 
+                    libero eos atque quia ipsa sint voluptatibus! 
+                    Beatae sit assumenda asperiores iure at maxime atque repellendus maiores quia sapiente.
+                </p>
+                <a href="#" class="readMoreBTN read-more">Read More</a>
+            </div>
+        </div>
+
+        <div class="post">
+            <img src="img/article1.png" alt="" class="post-image">
+            <div class="post-preview">
+                <h2><a href="#">Title Text Text Text</a></h2>
+                <i class="far calendar">Jul 31, 2021</i>
+                <p class="preview-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolor suscipit 
+                    libero eos atque quia ipsa sint voluptatibus! 
+                    Beatae sit assumenda asperiores iure at maxime atque repellendus maiores quia sapiente.
+                </p>
+                <a href="#" class="readMoreBTN read-more">Read More</a>
+            </div>
+        </div>
+
+        <div class="post">
+            <img src="img/article1.png" alt="" class="post-image">
+            <div class="post-preview">
+                <h2><a href="#">Title Text Text Text</a></h2>
+                <i class="far calendar">Jul 31, 2021</i>
+                <p class="preview-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolor suscipit 
+                    libero eos atque quia ipsa sint voluptatibus! 
+                    Beatae sit assumenda asperiores iure at maxime atque repellendus maiores quia sapiente.
+                </p>
+                <a href="#" class="readMoreBTN read-more">Read More</a>
+            </div>
+        </div>
+
+        <div class="post">
+            <img src="img/article1.png" alt="" class="post-image">
+            <div class="post-preview">
+                <h2><a href="#">Title Text Text Text</a></h2>
+                <i class="far calendar">Jul 31, 2021</i>
+                <p class="preview-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolor suscipit 
+                    libero eos atque quia ipsa sint voluptatibus! 
+                    Beatae sit assumenda asperiores iure at maxime atque repellendus maiores quia sapiente.
+                </p>
+                <a href="#" class="readMoreBTN read-more">Read More</a>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="sidebar"></div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
