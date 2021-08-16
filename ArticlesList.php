56634f7f1bb44e34351fadbf8a31951f130005f4
yuvalshai95 <62174154+yuvalshai95@users.noreply.github.com>
2021-08-16 17:23:20 +0300

    Articlelist pagination with css

--- ArticlesList.php
+++ ArticlesList.php
@@ -9,6 +9,8 @@
     <link href="styleA/css/all.min.css" rel="stylesheet">
     <!-- style Articles List -->
     <link href="styleA/articlesListStyle.css" rel="stylesheet">
+    <!-- style pagination  -->
+    <link href="styleA/pagination.css" rel="stylesheet">
 </head>
 <body>
     
@@ -18,78 +20,85 @@
 <div class="content clearfix">
     <div class="main-content">
         <h1 class="recent-post-title">Recent Articles</h1>
+        <?php 
+             $db = new Database();
+             $format = new Foramt();
+
+            // Define how many results you want per page
+            $result_per_page = 2;
+
+            // Find out the numberof results stored in database
+            $article = new Article();
+            $result = $article->getAllArticles();
+            $number_of_results = mysqli_num_rows($result);
+
+            // Determine number of total pages available
+            $number_of_pages = ceil($number_of_results/$result_per_page);
+
+            // Determine which page number visitor is currently on
+            if (!isset($_GET['page'])) {
+                 $page = 1;
+            }else{
+                $page = $_GET['page'];
+            }
+
+            // Determine the sql LIMIT starting number for the resutls on the displaying page
+            $this_page_first_result = ($page-1)*$result_per_page;
+
+             // Retrieve selected results from database and display them on page
+             $query  = "SELECT articles.*, articles_images.ImagePath
+             FROM articles
+             INNER JOIN articles_images ON articles.ImageRefrence  = articles_images.ImageRefrence
+             GROUP BY articles_images.ImageRefrence
+             ORDER BY ArticleTimeStamp DESC
+             LIMIT " . $this_page_first_result . ',' . $result_per_page;
+             $result = $db->select($query);
+
+             while ($row = $result->fetch_assoc()) {
+        ?>
 
         <div class="post">
-            <img src="img/article1.png" alt="" class="post-image">
+            <img src="admin/<?php echo $row['ImagePath'];?>" alt="" class="post-image">
             <div class="post-preview">
-                <h2><a href="#" class="par ttl">Title Text Text Text</a></h2>
-                <i class="far calendar">Jul 31, 2021</i>
+                <h2><a href="#" class="par ttl"><?= $row['ArticleHeader']; ?></a></h2>
+                <i class="far calendar"><?= $row['ArticleTimeStamp']; ?></i>
                 <p class="preview-text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolor suscipit 
-                    libero eos atque quia ipsa sint voluptatibus! 
-                    Beatae sit assumenda asperiores iure at maxime atque repellendus maiores quia sapiente.
+                    <?php echo $format->textShorten($row['ArticleBody'],350) ?>
                 </p>
+                <!-- TODO: Make link work -->
                 <a href="#" class="readMoreBTN read-more">Read More</a>
             </div>
         </div>
+        <?php } ?>
 
-        <div class="post">
-            <img src="img/article1.png" alt="" class="post-image">
-            <div class="post-preview">
-                <h2><a href="#" class="par ttl">Title Text Text Text</a></h2>
-                <i class="far calendar">Jul 31, 2021</i>
-                <p class="preview-text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolor suscipit 
-                    libero eos atque quia ipsa sint voluptatibus! 
-                    Beatae sit assumenda asperiores iure at maxime atque repellendus maiores quia sapiente.
-                </p>
-                <a href="#" class="readMoreBTN read-more">Read More</a>
-            </div>
+        <div class="pagination">
+            <?php
+
+                if ($page == 1) {
+                    $prev = 1;
+                }else{
+                    $prev = $page-1;
+                }
+                if ($page == $number_of_pages) {
+                    $next = $number_of_pages;
+                }else{
+                    $next = $page+1;
+                }
+                
+                // Display Previous page
+                echo '<a href="ArticlesList.php?page='.$prev.'" style="color:black;">&laquo; Previous </a>';
+
+                // Display the links to the pages
+                for($page = 1;$page<=$number_of_pages;$page++){
+                    echo '<a href="ArticlesList.php?page='.$page.'" style="color:black;">' . $page . '</a> ';
+                }
+
+                // Display Next page
+                echo '<a href="ArticlesList.php?page='.$next.'" style="color:black;"> Next &raquo; </a>';
+            ?>
         </div>
-
-        <div class="post">
-            <img src="img/article1.png" alt="" class="post-image">
-            <div class="post-preview">
-                <h2><a href="#" class="par ttl">Title Text Text Text</a></h2>
-                <i class="far calendar">Jul 31, 2021</i>
-                <p class="preview-text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolor suscipit 
-                    libero eos atque quia ipsa sint voluptatibus! 
-                    Beatae sit assumenda asperiores iure at maxime atque repellendus maiores quia sapiente.
-                </p>
-                <a href="#" class="readMoreBTN read-more">Read More</a>
-            </div>
-        </div>
-
-        <div class="post">
-            <img src="img/article1.png" alt="" class="post-image">
-            <div class="post-preview">
-                <h2><a href="#" class="par ttl">Title Text Text Text</a></h2>
-                <i class="far calendar">Jul 31, 2021</i>
-                <p class="preview-text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolor suscipit 
-                    libero eos atque quia ipsa sint voluptatibus! 
-                    Beatae sit assumenda asperiores iure at maxime atque repellendus maiores quia sapiente.
-                </p>
-                <a href="#" class="readMoreBTN read-more">Read More</a>
-            </div>
-        </div>
-
-        <div class="post">
-            <img src="img/article1.png" alt="" class="post-image">
-            <div class="post-preview">
-                <h2><a href="#" class="par ttl">Title Text Text Text</a></h2>
-                <i class="far calendar">Jul 31, 2021</i>
-                <p class="preview-text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolor suscipit 
-                    libero eos atque quia ipsa sint voluptatibus! 
-                    Beatae sit assumenda asperiores iure at maxime atque repellendus maiores quia sapiente.
-                </p>
-                <a href="#" class="readMoreBTN read-more">Read More</a>
-            </div>
-        </div>
-
     </div>
+ 
 
     <div class="sidebar">
 
@@ -101,6 +110,8 @@
                 <input type="text" name="search-term" class="text-input" placeholder="Search...">
             </form>
         </div>
+
+
 
 
         <div class="section topics">
--- includes/navTop.php
+++ includes/navTop.php
@@ -40,7 +40,7 @@
                     <li> <a href="#">Home</a></li>
                     <li> <a href="#">Categories</a></li>
                     <li> <a href="#">Sell</a></li>
-                    <li> <a href="#">Articles</a></li>
+                    <li> <a href="ArticlesList.php">Articles</a></li>
                     <li> <a href="#">About Us</a></li>
                 </ul>
             </nav>
