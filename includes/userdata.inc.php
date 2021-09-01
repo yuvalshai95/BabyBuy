555c0d475efe49281a04994138e732b3b30336bc
yuvalshai95 <62174154+yuvalshai95@users.noreply.github.com>
2021-09-01 21:12:19 +0300

    adding map to user details modal

--- includes/userdata.inc.php
+++ includes/userdata.inc.php
@@ -6,10 +6,26 @@ $id = $_POST['user_id'];
 $getUser = $user->getUserById($id);
 $output = '';
 
+
 $output .= '<div class="table-responsive">
                 <table class="table table-bordered">';
 
 while ($row = $getUser->fetch_assoc()) {
+
+     // get user city and repalce all white spaces with "+" sign
+     $city = $row['City'];
+     $city = str_replace(" ","+",$city);
+
+     // get user address and repalce all white spaces with "+" sign
+     $address = $row['Address'];
+     $address = str_replace(" ","+",$address);
+
+     // combine user city and address
+     $full_address = $city ."+".$address;
+
+
+
+
     $output .='
         <tr>
             <td width="30%"><label for=""></label>Name</td>
@@ -30,9 +46,15 @@ while ($row = $getUser->fetch_assoc()) {
             <td width="30%"><label for=""></label>Mobile Phone</td>
             <td width = "70%">'.$row['PhoneNumber'].'</td>
         </tr>
+
+
+
     ';
 }
-$output .='     </table> 
+$output .='     </table>
+                    <div align="center" class="resp-container" style="display:block;">
+                        <iframe  src="https://maps.google.com/maps?q='.$full_address.'&output=embed" ></iframe>
+                    </div> 
             </div>'; 
 
 echo $output;
