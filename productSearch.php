8f2fff72ec8c36c05136941186086e0caf8ccca0
yuvalshai95 <62174154+yuvalshai95@users.noreply.github.com>
2021-09-03 19:35:40 +0300

    if product not found redirect to 404 Error page

--- productSearch.php
+++ productSearch.php
@@ -34,7 +34,7 @@ $fm = new Foramt();
 
 <div class="col-lg-12">
     <h4 class="text-center" style="font-weight: bold; margin-top:10px;" id="textChange">Products found with "<?= $key?>"</h4>
-    <h6 class="text-center" style="font-weight: bold; margin-top:10px; color:gray;"><a style="color: gray;" href="homePage.php">Home</a> > Search </h6>
+    <h6 class="text-center" style="font-weight: bold; margin-top:10px; color:gray;"> > Search </h6>
     <hr style="width: 55%;margin: .5em auto;">
     <div style="justify-content: center;" class="row" id="data">
 
@@ -92,7 +92,10 @@ $fm = new Foramt();
                 </div>
             </div>
         </div>
-        <?php }} ?>
+        <?php }}else{
+                header("location: 404.php");
+                exit();
+        } ?>
     </div>
 </body>
 
