5bea3fe58442bdaa53da4c82c697ec6f6714d335
yuvalshai95 <62174154+yuvalshai95@users.noreply.github.com>
2021-09-03 19:35:51 +0300

    css changes

--- 404.php
+++ 404.php
@@ -4,12 +4,14 @@
 <style>
     .notfound h2{ font-size: 80px; line-height: 130px;text-align: center;}
     .notfound h2 span{display: block; color: red; font-size: 150px;}
+    .notfound h4{text-align: center;margin:.5em;}
 </style>
 
     <div class="notfound">
         <h2> <span>404</span> Not Found! </h2>
+        <h4><a style="color: gray; text-decoration:none;" href="homePage.php">Home Page</a></h4>
      </div>
-
+     
 
     <!-- footer-->
     <?php include_once 'includes/footer.php'; ?>
