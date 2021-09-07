0eb13f06aeb6f07b34a763977f15d8442b760d93
yuvalshai95 <62174154+yuvalshai95@users.noreply.github.com>
2021-09-08 01:44:29 +0300

    wishlist css

--- wishlist.php
+++ wishlist.php
@@ -41,7 +41,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- jQuery CDN  DON'T TOUCH OR CODE WILL BREAK-->
+
+<!-- jQuery CDN  DON'T TOUCH OR CODE WILL BREAK-->
 <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 <!-- jQuery CDN  DON'T TOUCH OR CODE WILL BREAK-->
 
@@ -50,80 +51,67 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> 
 <!-- BOOTSTRAP DON'T TOUCH OR CODE WILL BREAK -->
+
+<link rel="stylesheet" href="styleA/wishlist.css">
+
     <title>Wishlist</title>
 </head>
 <body>
-    <!-- wishlist table -->
-<h1>My Wishlist</h1>
-<table>
 
-    <th>Product Name</th>
-    <th>Unit Price</th>
-    <th>Status</th>
-    <th>Seller Details</th>
-    <th>Product Page</th>
-    <th>Remove</th>
+    <div class="small-container wishlist-page">
+        <table>
+            <tr>
+                <!-- Table Header -->
+                <th>Product</th>
+                <th>Status</th>
+                <th>Seller Details</th>
+                <th>Product Page</th>
+            </tr>
 
-    <!-- Get all wishlist items by user ID from session-->
-    <?php
+            
+            <!-- Get all wishlist items by user ID from session-->
+            <?php
 
-    $getAllWishlist = $pd->getAllWishlist($currentUser);
-    if ($getAllWishlist !== false) {
-        while($row = $getAllWishlist->fetch_assoc()){
-            $product = $pd->getProductByIdAndUser($row['ProductID'],$row['UserID'])->fetch_assoc();
-    ?>
-    <tr id="tr_<?php echo $row['ID'] ?>">
-        <td>
-            <img src="<?php  
-                        $img = $pd->getSingleImagesByProductId($product['ProductID'])->fetch_assoc(); 
-                        $single = $img['ImagePath']; 
-                        $single = str_replace('../','',$single); 
-                        echo $single?>" alt="" height="70px;" width="70px;">
-                        
-            <?php echo $product['ProductName'] ?>
-        </td>
-        <td><?php echo $product['Price'] ?></td>
-        <td><?php echo $product['Status'] ?></td>
-        <td><button type="button" class="btn btn-info btn-xs view_data" name="view" class="view_data" id="<?php echo $row['UserID']; ?>">Details</button></td>
-        <td><button><a href="ProductPage.php?pdId=<?php echo $row['ProductID']?>&userId=<?php echo $row['UserID']?>&productCategory=<?php echo $product['ProductCategory']?>">Go to page</a></button></td>
-        <td>
-            <button type="button" onclick="delete_data('<?php echo $row['ID']?>')">Delete</button>
-        </td>
-       
-    </tr>
-    <?php }}else{
+            $getAllWishlist = $pd->getAllWishlist($currentUser);
+            if ($getAllWishlist !== false) {
+                while($row = $getAllWishlist->fetch_assoc()){
+                    $product = $pd->getProductByIdAndUser($row['ProductID'],$row['UserID'])->fetch_assoc();
+            ?>
+            <tr id="tr_<?php echo $row['ID'] ?>">
+                <td>
+                    <div class="wishlist-info">
+                        <img src="<?php  
+                                    $img = $pd->getSingleImagesByProductId($product['ProductID'])->fetch_assoc(); 
+                                    $single = $img['ImagePath']; 
+                                    $single = str_replace('../','',$single); 
+                                    echo $single?>">
+
+                        <div>
+                            <p><?php echo $product['ProductName'] ?></p>
+                            <small>Unit Price: $<?php echo $product['Price'] ?></small>
+                            <br>
+                            <div class="delete">
+                                <button type="button" onclick="delete_data('<?php echo $row['ID']?>')">Delete</button>
+                            </div>
+                        </div>              
+                    </div>
+                </td>
+                <td><?php echo $product['Status'] ?></td>
+                <td><button type="button" name="view" class="view_data" id="<?php echo $row['UserID']; ?>">Details</button></td>
+                <td><a class="goto" href="ProductPage.php?pdId=<?php echo $row['ProductID']?>&userId=<?php echo $row['UserID']?>&productCategory=<?php echo $product['ProductCategory']?>">Go to page</a></td>
+            </tr>
+            <?php }}else{
         echo '<div align="center"><h3 style="color:red;">Empty :(</h3></div>';
     } ?>
-    
-</table>
+        </table>
+    </div>
+
 </body>
 </html>
 
 
-
-
-<style>
-    table tr td{
-        color: gray;
-    }
-
-    table th{
-        color: black;
-    }
-    table{
-    margin: auto;
-    }
-    h1,h2{
-        text-align: center;
-    }
-</style>
-
-
-
-
-
     <!-- jQuery Script to delete item from wishlist table -->
-    <script>
+<script>
     function delete_data(id){
         jQuery.ajax({
             url: 'includes/wishlist.inc.php',
